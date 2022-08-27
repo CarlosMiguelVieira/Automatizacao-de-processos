9e30e1721c268137dd6895d3d683d680dd01d21b
Carlos Miguel <cm_vp@hotmail.com>
2022-08-27 15:32:53 -0300

    Update CM_MTMGarantias.py

--- CM_MTMGarantias.py
+++ CM_MTMGarantias.py
@@ -10,7 +10,7 @@ from src.ferramentas import mandar_email
 from datetime import date
 #%%%
 # path = r'C:\Users\cpinto\Desktop\MTMs'
-# # path = r'Q:\Risco\Novo Risco\Ativa Asset\FIA\Carteira Diaria - 2022-06'
+# # path = r'Q:\Carteira Diaria - 2022-06'
 
 # os.chdir(path)
 
@@ -29,8 +29,8 @@ list_ticker = []
 list_garantia =[]
 list_disagio = []
 list_ok = []
-dflista = pd.read_csv(r'Q:\Risco\Novo Risco\pythonrisco\Codigos\data\CM_MTM\IBXX.csv',index_col = False,sep=';', encoding='latin-1',names= [ 'Codigo',1,2,3,4,5])
-df = pd.read_csv(r'Q:\Risco\Novo Risco\pythonrisco\Codigos\data\MTM_export\MaximumTheoreticalMargin.csv', sep=';',index_col = False, names= [ 'MTM', 'Data', 'Minima', 'Maxima', 'Minima2', 'Ticker',"AUX"])
+dflista = pd.read_csv(r'Q:\CM_MTM\IBXX.csv',index_col = False,sep=';', encoding='latin-1',names= [ 'Codigo',1,2,3,4,5])
+df = pd.read_csv(r'Q:\MTM_export\MaximumTheoreticalMargin.csv', sep=';',index_col = False, names= [ 'MTM', 'Data', 'Minima', 'Maxima', 'Minima2', 'Ticker',"AUX"])
 
 
 dflista = dflista['Codigo'].loc[2:99].tolist()
@@ -106,7 +106,7 @@ for ticker in list:
 
 
 
-dfListaGarantia = pd.read_excel(r'Q:\Risco\Novo Risco\pythonrisco\Codigos\data\CM_MTM\Aceitagarantia.xlsx',index_col = False)
+dfListaGarantia = pd.read_excel(r'Q:\CM_MTM\Aceitagarantia.xlsx',index_col = False)
 dfListaGarantia = dfListaGarantia['Código'].tolist()
 
 for x in list_ticker:
@@ -123,12 +123,12 @@ for x in list_ticker:
 dfl = pd.DataFrame((zip(list_ticker,list_garantia,list_disagio,list_ok)), columns = ['Ticker','Garantia','Desagio','Aceito como garantia?'])
 dfl['Aceito como garantia?']=dfl['Aceito como garantia?'].replace(True,'Sim').replace(False,'Não')
 
-dfl.round(2).to_excel(r'Q:\Risco\Novo Risco\pythonrisco\Codigos\data\CM_MTM\Valor Minimo em Garantia.xlsx', sheet_name='MTM', index=False)
+dfl.round(2).to_excel(r'Q:\CM_MTM\Valor Minimo em Garantia.xlsx', sheet_name='MTM', index=False)
 # %%
 today=date.today()
 data = today.strftime('%d/%m/%Y')
-endereco = "options@ativainvestimentos.com.br;mesainstrj@ativainvestimentos.com.br;comercialbh@ativainvestimentos.com.br>;comercialrj@ativainvestimentos.com.br;comercialrs@ativainvestimentos.com.br;comercialsp@ativainvestimentos.com.br;comercialpr@ativainvestimentos.com.br;comerciais@ativainvestimentos.com.br;atendimentorj@ativainvestimentos.com.br;custodia@ativainvestimentos.com.br;comercialba@ativainvestimentos.com.br"
-endereco_cc = "risco@ativainvestimentos.com.br"
+endereco = ""
+endereco_cc = "cm_vp@hotmail.com"
 titulo = "Valor mínimo em garantia - " + str(data)
 mensagem = ("<p>Prezados,</p>"
        "<p>      Segue em anexo a análise da carteira do IBX-100 em relação valor mínimo em garantia por ação. </p>"
@@ -137,7 +137,7 @@ mensagem = ("<p>Prezados,</p>"
        "<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp• Deságio = Diferença entre o valor real e o nominal do ativo  </p>"
        "<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp• Aceito em garantia = Possibilidade do ativo ser alocado em garantia</p>"
     )
-anexo = r'Q:\Risco\Novo Risco\pythonrisco\Codigos\data\CM_MTM\Valor Minimo em Garantia.xlsx'
+anexo = r'Q:\CM_MTM\Valor Minimo em Garantia.xlsx'
 mandar_email(endereco,endereco_cc,titulo,mensagem,anexo)
 
 
